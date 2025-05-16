46de77856ea13aa005606f66dd792a6123ef2196
prasanv <prasan.venkatb@gmail.com>
2025-05-15 22:16:16 -0700

    imported and exported modules

--- src/power/power.module.ts
+++ src/power/power.module.ts
@@ -3,6 +3,8 @@ import { PowerService } from './power.service';
 import { RegulatorService } from './regulator/regulator.service';
 
 @Module({
+  /* By default the services of each module are private, until exported can't be accessed by other module */
   providers: [PowerService, RegulatorService],
+  exports: [PowerService],
 })
 export class PowerModule {}
